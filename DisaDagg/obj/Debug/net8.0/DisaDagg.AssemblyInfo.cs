984874df944a13e0579c370f7969fbbc154475ee
Pär Hedström <h24parhe@du.se>
2024-12-18 16:19:01 +0100

    .

--- DisaDagg/obj/Debug/net8.0/DisaDagg.AssemblyInfo.cs
+++ DisaDagg/obj/Debug/net8.0/DisaDagg.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DisaDagg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ba195acb3c535fd822dda72c8f017b92d636e71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+021d81710fe88113f91300a0e081d20dfd9744b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DisaDagg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DisaDagg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

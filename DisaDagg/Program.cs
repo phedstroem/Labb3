b67e8f55f1c9dc2c74abf71b9a9128e4ceec13f5
Fredrik Domert Eriksson <fredrik.domert@gmail.com>
2024-12-18 15:00:52 +0100

    .

--- DisaDagg/Program.cs
+++ DisaDagg/Program.cs
@@ -31,9 +31,73 @@ class Program
         int averageTemp = CalcTemp.AverageTemp(mayTemps);
         Console.WriteLine(averageTemp + " °C");
 
+        // Test för att skriva ut specifik dag
         Console.WriteLine($"Temp: {mayTemps[7].Temp} Dag: {mayTemps[7].Day}");
 
 
 
+        bool isRunning = true;
+        while (isRunning)
+        {
+            Console.WriteLine("Hej Disa Dagg ");
+            Console.WriteLine("Här ska du få lite hjälp med temperaturer för Maj");
+            Console.WriteLine("\n1.  Skriv ut samtliga temperaturer");
+            Console.WriteLine("2.  Medeltemperaturen");
+            Console.WriteLine("3.  Högsta temperaturen"); // och dag
+            Console.WriteLine("4.  Lägsta temperaturen "); // och dag
+            Console.WriteLine("5.  Mediantemperatur");
+            Console.WriteLine("6.  Sortera temperaturerna");
+            Console.WriteLine("7.  Filtrera som överstiger XX");
+            Console.WriteLine("8.  Väl en dag och visa temp dag före/efter");
+            Console.WriteLine("9.  Visa vanligast förekommande temperatur");
+            Console.WriteLine("10. Avsluta");
+            Console.WriteLine("Vad vill du göra?");
+            string userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "1":
+                    Console.Clear();
+                    Console.Write("\nTryck för att fortsätta...");
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+                case "2":
+                    Console.Clear();
+                    Console.Write("\nTryck för att fortsätta...");
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+                case "3":
+                    Console.Clear();
+                    Console.Write("\nTryck för att fortsätta...");
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    break;
+                case "7":
+                    break;
+                case "8":
+                    break;
+                case "9":
+                    break;
+                case "10":
+                    Console.WriteLine("Du valde att avsluta");
+                    isRunning = false;
+                    break;
+                default:
+                    Console.Clear();
+                    Console.Write("Ogiltig inmatning, välj alternativ 1-10. Tryck för att fortsätta...");
+                    Console.ReadKey();
+                    break;
+                
+
+            }
+        }
     }
 }

c84b7cd016ee8cb3e87181672d1a3bed0cea0e27
Fredrik Domert Eriksson <fredrik.domert@gmail.com>
2024-12-18 16:20:38 +0100

    .

--- DisaDagg/obj/Debug/net8.0/DisaDagg.AssemblyInfo.cs
+++ DisaDagg/obj/Debug/net8.0/DisaDagg.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DisaDagg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ba195acb3c535fd822dda72c8f017b92d636e71")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd18f0f7fe1ebf19eab457fd923466f1ec811eea")]
+>>>>>>> parent of 06f1cc5 (.)
 [assembly: System.Reflection.AssemblyProductAttribute("DisaDagg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DisaDagg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
